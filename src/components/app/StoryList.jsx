220bb0cbc213922f7c48f1f832281061975cbd00
Isaac <isaac.ewing3@gmail.com>
2021-09-15 18:18:00 -0600

    tests work

--- src/components/app/StoryList.jsx
+++ src/components/app/StoryList.jsx
@@ -3,23 +3,23 @@ import PropTypes from 'prop-types';
 import Story from './Story';
 
 const StoryList = ({ stories }) => (
-    <ul aria-label="stories">
-        {stories.map(({ author, title, description }) => (
-            <li key={`${author}-${title}`}>
-                <Story author={author} title={title} description={description} />
-            </li>
-        ))}
-    </ul>
+  <ul aria-label="stories">
+    {stories.map(({ author, title, description }) => (
+      <li key={`${author}-${title}`}>
+        <Story author={author} title={title} description={description} />
+      </li>
+    ))}
+  </ul>
 );
 
 StoryList.propTypes = {
-    stories: PropTypes.arrayOf(
-        PropTypes.shape({
-            author: PropTypes.string.isRequired,
-            title: PropTypes.string.isRequired,
-            description: PropTypes.string.isRequired,
-        })
-    ).isRequired,
-}
+  stories: PropTypes.arrayOf(
+    PropTypes.shape({
+      author: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default StoryList;
--- src/services/newsAPI.jsx
+++ src/services/newsAPI.jsx
@@ -1,13 +1,13 @@
 export const formatStory = (res) => 
-    // console.log(res.articles);
-    res.articles.map((story) => ({
-        author: story.author,
-        title: story.title,
-        description: story.description
-    }));
+// console.log(res.articles);
+  res.articles.map((story) => ({
+    author: story.author,
+    title: story.title,
+    description: story.description
+  }));
 
 export const getNewsWithQuery = async (query) => {
-    const res = await fetch(`http://newsapi.org/v2/everything?qInTitle=${query}&apiKey=${process.env.NEWS_API_KEY}`);
-    const json = await res.json();
-    return formatStory(json);
+  const res = await fetch(`https://newsapi.org/v2/everything?qInTitle=${query}&apiKey=${process.env.NEWS_API_KEY}`);
+  const json = await res.json();
+  return formatStory(json);
 }
